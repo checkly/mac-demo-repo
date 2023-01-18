6904a66dc57afb15589eaed1f7cb2ec0477ffd2f
tnolet <tim@checklyhq.com>
2023-01-18 21:16:13 +0000

    feat: reshuffles checks, adds docs, pages and apis

---
+++ pages/__checks__/product.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test')
+const { defaults } = require('../../__checks__/defaults')
+
+test('Mac Demo Product Page', async ({ page }) => {
+  const response = await page.goto(defaults.pageUrl)
+  expect(response.status()).toBeLessThan(400)
+  await expect(page).toHaveTitle(/MaC/)
+  await page.screenshot({ path: 'about.jpg' })
+})
